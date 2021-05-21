d76ee68844cf7588d7203dab8311e06cda1307ee
Saurav Dutta <saurav@piktorlabs.com>
2021-05-21 15:34:24 +0530

    changes

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ var io = require('socket.io')(http);
 const STATIC_CHANNELS = [{
     name: 'Hip Hop',
     participants: 0,
-    id: 1,
+    id: 101,
     sockets: []
 }, {
     name: 'EDM',
     participants: 0,
-    id: 2,
+    id: 102,
     sockets: []
 }];
 /**
@@ -29,12 +29,51 @@ app.use(cors());
 io.on('connection', (socket) => { /* socket object may be used to send specific messages to the new connected client */
     console.log('new client connected');
     socket.emit('connection', null);
+
+    socket.on('join-room', room => {
+        console.log('joined a room', room)
+        socket.join(room)
+        STATIC_CHANNELS.forEach(c => {
+            if (c.id === room) {
+                if (c.sockets.indexOf(socket.id) == (-1)) {
+                    c.sockets.push(socket.id);
+                    c.participants++;
+                    io.emit('channel', c);
+                }
+            } else {
+                let index = c.sockets.indexOf(socket.id);
+                if (index != (-1)) {
+                    c.sockets.splice(index, 1);
+                    c.participants--;
+                    io.emit('channel', c);
+                }
+            }
+        });
+    });
+
+    socket.on('leave-room', room => {
+        console.log('leave a room', room)
+        socket.leave(room)
+        STATIC_CHANNELS.forEach(c => {
+            if (c.id === room) {
+                let index = c.sockets.indexOf(socket.id);
+                c.sockets.splice(index, 1);
+                c.participants--;
+                io.emit('channel', c);
+            }
+        });
+    });
+
+    socket.on('voting', ({room, votingData}) => {
+        socket.to(room).emit('voting', {
+            votingData
+        })
+    });
+
     socket.on('channel-join', id => {
         console.log('channel join', id);
         STATIC_CHANNELS.forEach(c => {
             if (c.id === id) {
-                console.log(c)
-                console.log(socket.id)
                 if (c.sockets.indexOf(socket.id) == (-1)) {
                     c.sockets.push(socket.id);
                     c.participants++;
